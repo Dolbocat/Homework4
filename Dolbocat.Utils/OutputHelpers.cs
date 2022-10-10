9a2d6ec659f34bb753b968af24a1e5e49a899bb5
Dolbocat <Delorkin@yandex.ru>
2022-10-10 19:12:50 +0300

    Домашняя работа 4

---
+++ Dolbocat.Utils/OutputHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolbocat.Utils
+{
+    public class OutputHelpers
+    {
+        public static void PrintInfo(int homeworknumber, string fio)
+        {
+            Console.WriteLine($"Домашняя работа. урок: {homeworknumber}");
+            Console.WriteLine($"Студент {fio}");
+            Console.WriteLine("==========================================\n");
+        }
+
+    }
+}
--- Homework4/ArrayClass.cs
+++ Homework4/ArrayClass.cs
@@ -8,5 +8,6 @@ namespace Homework4
 {
     internal class ArrayClass
     {
+
     }
 }
